d70e5b8cb7d7fe2b75f1ef870719058dac4d1f85
paulogabriellimit-hue <paulogabriel.limit@hcdc.edu.ph>
2026-03-15 12:59:57 +0800

    Create themecontext.ts

---
+++ app/(tabs)/themecontext.ts
@@ -0,0 +1,27 @@
+import React, { createContext, useState, ReactNode } from "react";
+
+type ThemeType = "light" | "dark";
+
+interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<ThemeType>("light");
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
