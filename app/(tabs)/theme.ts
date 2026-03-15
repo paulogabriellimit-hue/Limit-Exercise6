a7b833e3c07378bb6e7dd9567df6e38b68aca663
paulogabriellimit-hue <paulogabriel.limit@hcdc.edu.ph>
2026-03-15 13:01:16 +0800

    Initial commit

---
+++ app/(tabs)/theme.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  background: "#ffffff",
+  text: "#000000",
+  button: "#007AFF",
+};
+
+export const darkTheme = {
+  background: "#121212",
+  text: "#ffffff",
+  button: "#BB86FC",
+};
